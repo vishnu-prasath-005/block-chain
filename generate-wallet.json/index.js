066c94e6bad905dd6100ac8dca1db08fede303b4
vishnu-prasath-005 <vishnu2032002@gmail.com>
2024-11-13 13:05:08 +0530

    generate wallet fixes

--- generate-wallet.json/index.js
+++ generate-wallet.json/index.js
@@ -3,13 +3,13 @@ const fs = require("fs");
 const ethers = require("ethers");
 
 const wallet = new ethers.Wallet(
-  "c8dc090c906f78a074caeab97218b7b32a5a9c4606a2996f962a35de67959024"
+  "f637a31dd69d0a6ea6f2d30c2f255e45217bc39f318b4bac31eacfb2e0289dcd"
 );
 
 const walletPassword = "12345qwerty";
 (async () => {
   const walletJson = await wallet.encrypt(walletPassword);
-  fs.writeFileSync("wallet.json", JSON.stringify(walletPassword));
+  fs.writeFileSync("wallet.json", walletJson);
   console.log("wallet as json =  ", walletJson);
   const decrypt = await ethers.decryptKeystoreJson(walletJson, walletPassword);
   console.log("\n decrypted wallet = ", decrypt);
